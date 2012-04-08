7a62de3211690c10dc21269d223a905a6226e015
abhat@J9978R1-ABHAT.corp.zynga.com <abhat@J9978R1-ABHAT.corp.zynga.com>
2012-04-08 20:02:44 +0530

    First check in of minimum cut.

---
+++ creategraph.py
@@ -0,0 +1,66 @@
+import pprint
+import random
+
+def contract(graph, vertex0, vertex1):
+    adjecents0 = graph[vertex0]
+    adjecents1 = graph[vertex1]
+    adjecents = adjecents0 + adjecents1
+    vertex = "{0}-{1}".format(vertex0, vertex1)
+    del graph[vertex0]
+    del graph[vertex1]
+    graph[vertex] = adjecents
+
+    for adjecentList in graph.values():
+        if vertex0 in adjecentList:
+            count = adjecentList.count(vertex0)
+
+            while count:
+                del adjecentList[adjecentList.index(vertex0)]
+                adjecentList.append(vertex)
+                count = count - 1
+
+        if vertex1 in adjecentList:
+            count = adjecentList.count(vertex1)
+
+            while count:
+                del adjecentList[adjecentList.index(vertex1)]
+                adjecentList.append(vertex)
+                count = count - 1
+
+    for key, value in graph.iteritems():
+        if key in value:
+            #print "key is:", key
+            #pprint.pprint(value)
+            count = value.count(key)
+            while count:
+                del value[value.index(key)]
+                count = count - 1
+
+fh = open('kargerAdj.txt')
+
+lines = fh.readlines()
+
+graph = {}
+for line in lines:
+    vertices = line.split()
+    graph[vertices[0]] = vertices[1:]
+
+pprint.pprint(graph)
+
+while len(graph.keys()) > 2:
+    randVertex0 = graph.keys()[random.randint(0, len(graph.keys()) - 1)]
+    adjecents = graph[randVertex0]
+    randVertex1 = adjecents[random.randint(0, len(adjecents) - 1)]
+
+    #print "Contraction candidates: {0} - {1}".format(randVertex0, randVertex1)
+    #pprint.pprint(graph)
+
+    contract(graph, randVertex0, randVertex1)
+
+#pprint.pprint(graph)
+
+minimumCuts = 0
+for values in graph.values():
+    minimumCuts = minimumCuts + len(values)
+
+print minimumCuts

a48e105bdd7589e0f30afc379ea45ca0f013532a
abhat@J9978R1-ABHAT.corp.zynga.com <abhat@J9978R1-ABHAT.corp.zynga.com>
2012-04-15 23:40:25 +0530

    Added timing logic.

--- week4/scc.py
+++ week4/scc.py
@@ -20,13 +20,47 @@ for line in lines:
     line = line.strip()
     vertices = line.split()
 
-    addToMap(fMap, vertices[0], vertices[1])
-    addToMap(rMap, vertices[1], vertices[0])
+    v0 = long(vertices[0])
+    v1 = long(vertices[1])
 
-    mxVert = max(mxVert, vertices[0], vertices[1])
+    addToMap(fMap, v0, v1)
+    addToMap(rMap, v1, v0)
 
-pprint.pprint(fMap)
-print "------------------------"
-pprint.pprint(rMap)
-print "------------------------"
-print 'Max is:', mxVert
+    mxVert = max(mxVert, v0, v1)
+
+
+_timer = 0
+def dfs(vert, seen, timeMap):
+    if vert in seen:
+        return False 
+        
+    seen.add(vert)
+
+    global _timer
+    if vert in fMap:
+        edges = fMap[vert] 
+        for edge in edges:
+            if not dfs(edge[1], seen, timeMap):
+                _timer = _timer + 1
+                timeMap[vert] = _timer
+
+        if not vert in timeMap:
+            _timer = _timer + 1
+            timeMap[vert] = _timer
+        return True
+    else:
+        return False 
+
+
+
+seen = set()
+timeMap = {}
+
+for vert in reversed(xrange(1, mxVert + 1)):
+   if not dfs(vert, seen, timeMap):
+        if not vert in timeMap:
+            _timer = _timer + 1
+            timeMap[vert] = _timer
+
+
+pprint.pprint(timeMap)

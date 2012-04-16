566532ac64c077be1c577fc67f68bef86b5f6292
abhat@J9978R1-ABHAT.corp.zynga.com <abhat@J9978R1-ABHAT.corp.zynga.com>
2012-04-16 22:44:16 +0530

    Stack size and memory limit changes. Also, the vertex count is not considered continuous.

--- week4/scc.py
+++ week4/scc.py
@@ -1,4 +1,8 @@
 import pprint
+import sys
+import threading
+
+#Code has become very ugly. Looks like a hack job.
 
 def createGraph(mp, tail, head):
     if tail in mp: 
@@ -6,30 +10,8 @@ def createGraph(mp, tail, head):
     else:
         mp[tail] = [(tail, head)]
 
-fh = open('small.txt')
 
-lines = fh.readlines()
-
-fGraph = {}
-
-rGraph = {}
-
-mxVert = 0
-
-for line in lines:
-    line = line.strip()
-    vertices = line.split()
-
-    v0 = long(vertices[0])
-    v1 = long(vertices[1])
-
-    createGraph(fGraph, v0, v1)
-    createGraph(rGraph, v1, v0)
-
-    mxVert = max(mxVert, v0, v1)
-
-
-def dfsForTime(vert, seen, timeMap):
+def dfsForTime(vert, seen, vertTimeMap):
     if vert in seen:
         return False 
         
@@ -39,25 +21,93 @@ def dfsForTime(vert, seen, timeMap):
     if vert in fGraph:
         edges = fGraph[vert] 
         for edge in edges:
-            dfsForTime(edge[1], seen, timeMap)
+            dfsForTime(edge[1], seen, vertTimeMap)
 
         _timer = _timer + 1
-        timeMap[vert] = _timer
+        vertTimeMap[vert] = _timer
         return True
     else:
         return False 
 
 
+def dfsForGrouping(vert, seen, traversedVertices):
+    if vert in seen:
+        return False 
+        
+    seen.add(vert)
+    traversedVertices.add(vert)
 
-seen = set()
-timeMap = {}
+    if vert in rGraph:
+        edges = rGraph[vert] 
+        for edge in edges:
+            dfsForGrouping(edge[1], seen, traversedVertices)
+
+vertTimeMap = {}
+fGraph = {}
+rGraph = {}
 
 _timer = 0
-for vert in reversed(xrange(1, mxVert + 1)):
-   if not dfsForTime(vert, seen, timeMap):
-        if not vert in timeMap:
-            _timer = _timer + 1
-            timeMap[vert] = _timer
+
+def main():
+    fh = open('SCC1.txt')
+    #fh = open('tstCase0.txt')
+    #n = 12 
+    #print n, "=>"
+    #fh = open('PQ4GraphSCC12TestCases\\graph{0}.txt'.format(n))
+
+    lines = fh.readlines()
 
 
-pprint.pprint(timeMap)
+    mxVert = 0
+
+    for line in lines:
+        line = line.strip()
+        vertices = line.split()
+
+        v0 = long(vertices[0])
+        v1 = long(vertices[1])
+
+        if v0 == v1:
+            continue
+
+        createGraph(fGraph, v0, v1)
+        createGraph(rGraph, v1, v0)
+
+        mxVert = max(mxVert, v0, v1)
+
+    global _timer
+
+    seen = set()
+    for vert in reversed(xrange(1, mxVert + 1)):
+        if vert not in fGraph:
+            continue
+
+        if not dfsForTime(vert, seen, vertTimeMap):
+            if not vert in vertTimeMap:
+                _timer = _timer + 1
+                vertTimeMap[vert] = _timer
+
+
+    timeVertMap = dict([[v,k] for k,v in vertTimeMap.items()])
+
+    seen = set()
+    sccSize = []
+
+    maxTime = max(timeVertMap.keys())
+    for time in reversed(xrange(1, maxTime + 1)):
+        vert = timeVertMap[time]
+        traversedVertices = set()
+        dfsForGrouping(vert, seen, traversedVertices)
+        sccSize.append(len(traversedVertices))
+
+    sccSize.sort()
+    sccSize.reverse()
+
+    pprint.pprint(sccSize[0:5])
+
+if __name__=="__main__":
+    sys.setrecursionlimit(100000)
+    threading.stack_size(128 * 1024 * 1024)    # 128 MB 
+    thread = threading.Thread(target=main)
+    thread.start()
+    thread.join(0)
